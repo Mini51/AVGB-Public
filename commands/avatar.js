4bfb3cbee567a396ec0c406c1b6f401a8c99190c
Mini <80361877+youthfulslinky@users.noreply.github.com>
2021-10-22 22:18:16 -0400

    fixed requirement ✅

--- commands/avatar.js
+++ commands/avatar.js
@@ -10,7 +10,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
 		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
-		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show').setRequired(false)),
+		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show').setRequired(true)),
 	async execute(interaction) {
 
 		// Getting the user that was mentioned
