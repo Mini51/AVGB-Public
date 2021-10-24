0a31e37dd8a651c843f1bd60892c5f04335ec48f
Mini <80361877+youthfulslinky@users.noreply.github.com>
2021-10-23 20:41:30 -0400

    added credit

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,8 @@
+/* original code
+https://github.com/discordjs/guide/blob/main/code-samples/creating-your-bot/creating-commands/deploy-commands.js
+*/  
+
+
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
